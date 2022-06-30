b2700db7c5be80525da61b6d9677ddea6062fa47
Lodoctor <75381404+Evanslodoctor@users.noreply.github.com>
2022-06-30 01:51:14 -0700

    Add files via upload

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main-Entry point with puts function which prints string followed by new line.
+ *
+ *Return:Always 0(Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
