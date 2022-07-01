b6d82599fabfa4abdb5c82a020430584cc165360
Lodoctor <75381404+Evanslodoctor@users.noreply.github.com>
2022-07-01 02:51:16 -0700

    Add files via upload

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char low;
+
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+printf("\n");
+
+return (0);
+}
