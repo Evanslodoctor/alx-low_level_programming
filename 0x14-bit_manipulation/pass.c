5728cd35ebabd31647485825d59a90e4cb627ea3
Evans Chaun <75381404+Evanslodoctor@users.noreply.github.com>
2023-10-10 10:38:19 +0300

    Add files via upload

---
+++ 0x14-bit_manipulation/pass.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * pass - function that prints characters with _putcharr
+ * Return: is Always 0
+ */
+
+int pass(void)
+{
+	int x = 0x48;
+	int y = 0x6F;
+	int z = 0x6C;
+	int p = 0x04;
+
+	_putcharr(x);
+	return (0);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This directory contains I/O tasks
