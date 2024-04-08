12f175d3c38c1755c17e64a1606bb9b4d7f3c6cd
Evans Chaun <75381404+Evanslodoctor@users.noreply.github.com>
2024-04-08 17:07:25 +0300

    Add files via upload

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,55 +1,39 @@
 #include "search_algos.h"
-#include <math.h>
 
 /**
- * jump_search - searches for a value in a sorted array of integers
- * using the Jump search algorithm
- * @array: array to look into
- * @size: size of the array
- * @value: value to look for
- *
- * Return: index of the value found, or -1
- */
+  * jump_search - Searches for a value in a sorted array
+  *               of integers using jump search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the first index where the value is located.
+  *
+  * Description: Prints a value every time it is compared in the array.
+  *              Uses the square root of the array size as the jump step.
+  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t start, end, step;
+	size_t i, jump, step;
 
-	if (!array || size == 0)
+	if (array == NULL || size == 0)
 		return (-1);
+
 	step = sqrt(size);
-	start = 0, end = step;
-
-	while (start < size)
+	for (i = jump = 0; jump < size && array[jump] < value;)
 	{
-		printf("Value checked array[%lu] = [%d]\n", start, array[start]);
-		if (end < size)
-		{
-			if (array[start] <= value && value <= array[end])
-			{
-				printf("Value found between indexes [%lu] and [%lu]\n", start, end);
-				break;
-			}
-		} else
-		{
-			if (array[start] <= value)
-			{
-				printf("Value found between indexes [%lu] and [%lu]\n", start, end);
-				break;
-			}
-		}
-		start = end;
-		end = start + step;
+		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
+		i = jump;
+		jump += step;
 	}
 
-	while (start <= end)
-	{
-		if (start == size)
-			return (-1);
-		printf("Value checked array[%lu] = [%d]\n", start, array[start]);
-		if (array[start] == value)
-			return (start);
-		start++;
-	}
+	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
 
-	return (-1);
+	jump = jump < size - 1 ? jump : size - 1;
+	for (; i < jump && array[i] < value; i++)
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+	return (array[i] == value ? (int)i : -1);
 }
--- 0x1E-search_algorithms/README.md
+++ 0x1E-search_algorithms/README.md
@@ -1,13 +1,19 @@
-# 0x1E. C - Search Algorithms
+# Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
 
-[0-linear.c](./0-linear.c) | C function that searches for a value in an array of integers using the Linear search algorithm
-[1-binary.c](./1-binary.c) | C function that searches for a value in a sorted array of integers using the Binary search algorithm
-[2-O](./2-O) | What is the time complexity (worst case) of a linear search in an array of size n?
-[3-O](./3-O) | What is the space complexity (worst case) of an iterative linear search algorithm in an array of size n?
-[4-O](./4-O) | What is the time complexity (worst case) of a binary search in an array of size n?
-[5-O](./5-O) | What is the space complexity (worst case) of a binary search in an array of size n?
-[6-O](./6-O) | What is the space complexity of this function / algorithm?
-[100-jump.c](./100-jump.c) | C function that searches for a value in a sorted array of integers using the Jump search algorithm
-[101-O](./101-O) | What is the time complexity (average case) of a jump search in an array of size n, using step = sqrt(n)?
-[107-O](./107-O) | What is the time complexity (average case) of a jump search in a singly linked list of size n, using step = sqrt(n)?
-[108-O](./108-O) | What is the time complexity (average case) of a jump search in a skip list of size n, with an express lane using step = sqrt(n)?
+## General
+* What is a search algorithm
+* What is a linear search
+* What is a binary search
+* What is the best search algorithm to use depending on your needs
+
+### Tasks
+#### 0. Linear search
+Write a function that searches for a value in an array of integers using the Linear search algorithm
+
+* Prototype : int linear_search(int *array, size_t size, int value);
+* Where array is a pointer to the first element of the array to search in
+* size is the number of elements in array
+* And value is the value to search for
+* Your function must return the first index where value is located
+* If value is not present in array or if array is NULL, your function must return -1
